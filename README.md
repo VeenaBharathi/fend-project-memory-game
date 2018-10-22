bd21150b9070ff2a25168b3d3cb77952a32e5eb6
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-10-22 22:12:21 +0530

    FEND_PR2

--- README.md
+++ README.md
@@ -7,14 +7,76 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+1.
+2. The player has to find matching card sets, one pair after other.
+3. The first card that he flips will be the reference card and the second card will be checked against the first.
+4. If both match, they are locked. Else, the symbols on them will be hidden.
+5. The no of moves determine the star rating and the total score where 10 points for each move is counted.
+6. The lesser the score, the better the game has been.
+7. The lesser the stars, poor is the performance.
+8. A modal box with time up message is shown if the time limit ends before player wins the game. He can choose to play again.
+8. A modal box congratulating the user with score, moves and stars is shown once player wins the game in given time. He can chose to play over again.
+9. Refresh button is provided on the page if user wants to reload the game at any point.
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+Key Contributer - Veena
+Reviewers - Udcity team.
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+Project Title
+This is a memory Game project. It gives every player 60sec to complete the game.
+
+Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+Prerequisites
+What things you need to install the software and how to install them
+
+Give examples
+Installing
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+Give the example
+And repeat
+
+until finished
+End with an example of getting some data out of the system or using it for a little demo
+
+Running the tests
+Explain how to run the automated tests for this system
+
+Break down into end to end tests
+Explain what these tests test and why
+
+Give an example
+And coding style tests
+Explain what these tests test and why
+
+Give an example
+Deployment
+Add additional notes about how to deploy this on a live system
+
+Built With
+Dropwizard - The web framework used
+Maven - Dependency Management
+ROME - Used to generate RSS Feeds
+Contributing
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+Versioning
+We use SemVer for versioning. For the versions available, see the tags on this repository.
+
+Authors
+Billie Thompson - Initial work - PurpleBooth
+See also the list of contributors who participated in this project.
+
+License
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+Acknowledgments
+Hat tip to anyone whose code was used
+Inspiration
+etc
+
--- js/app.js
+++ js/app.js
@@ -1,38 +1,191 @@
-/*
- * Create a list that holds all of your cards
- */
+/* Create a list that holds all of your cards*/
+var time = 60;
+var stop = 0;
+let rates = 3;
+onTimer();
+let cards = document.querySelectorAll(".deck .card");
+let shuffledCards = shuffle.call(cards);
+rearrangeLi.call(shuffledCards);
+let openCards = [];
+let count =0;
+let moves = 0;
+let locks = 0;
 
+// start timer
+function onTimer() {
+	if(time <10)
+  		document.querySelector("#counter").innerHTML = "0" + time;
+	else
+	 document.querySelector("#counter").innerHTML = time;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+  	if(time === 0 && stop == 0) {
+	  	document.querySelector("#counter").innerHTML = "0" + time;
+	  	document.querySelector("#mod").style.display = "block";
+	  	document.querySelector(".congos").innerHTML =  "Time Up. You Lost!!"
+	}
+  	else if(time != 0 && stop == 0) {
+	  	time--;
+	    setTimeout(onTimer, 1000);
     }
-
-    return array;
 }
 
+// shuffle the available cards
+function shuffle() {
+	 let cards = this;
+     let arr = [];
+     let cardsArr = Array.prototype.slice.call(cards);
+     for (var i = 0; i <= cardsArr.length; i++) {
+     	if(cardsArr.length === i) {
+	            	i = 0;
+     	}
+        let index = Math.floor(Math.random()*cardsArr.length);
+        let item = cardsArr[index];
+        arr.push(item);
+        cardsArr.splice(index,1);
+	 }
+     return arr;
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// re-arrange shuffled cards and them to ul
+function rearrangeLi() {
+	document.querySelector('.deck').remove('li');
+	let list =	document.createElement('ul');
+	list.classList = "deck";
+	document.querySelector('.container').appendChild(list);
+	let listEle = document.querySelector('.deck');
+
+	for(let j = 0; j < shuffledCards.length; j++) {
+	listEle.appendChild(shuffledCards[j]);
+	}
+ }
+
+// add event listener to the cards on click
+var cardList = document.querySelectorAll('.deck li');
+for(let i =0; i<cardList.length; i++)
+{
+   cardList[i].addEventListener('click', function(e)
+   {
+		if(e.target && e.target.matches(".card"))
+		{
+			if(!this.classList.contains("match")){
+            	displaySymb.call(this);
+           		setTimeout(function(){
+           		addToOpenCards.call(e.target)},500);
+           	}
+        }
+	});
+}
+
+// displays symbols when card is clicked.
+function displaySymb() {
+		let card = this;
+		card.classList.toggle("open");
+		card.classList.toggle("show");
+}
+
+// adds cards to open cards list and check for the matching cards when card ic clicked
+function addToOpenCards(){
+
+       count++;
+       let pick = this;
+       openCards.push(this);
+
+       if(openCards.length>1){
+			for(let x = 1; (x <= openCards.length && x != count); x++) {
+				   	if(this != openCards[x-1]) {
+				   		moves++;
+                			// if cards match, lock the cards
+							if(this.innerHTML === openCards[x-1].innerHTML){
+								lockCards.call(this, openCards[x-1]);
+							}
+							// if cards dont match, hide the card
+							else {
+								openCards[x-1].classList.add("wrong");
+								this.classList.add("wrong");
+
+								openCards[x-1].classList.remove("show","open");
+								this.classList.remove("show","open");
+
+								let c = openCards[x-1];
+
+								setTimeout(function() {
+									pick.classList.remove("wrong");
+									c.classList.remove("wrong");
+								},500);
+
+								openCards = [];
+								count = 0;
+							}
+					}
+					else{
+						// moves--;
+						break;
+					}
+				count = 0;
+			}
+		openCards=[];
+		}
+	updateMovesAndStars();
+}
+
+//lock cards when match is found
+function lockCards(currcard) {
+	locks++;
+	let ref = this;
+	let openCard = currcard;
+	ref.classList.add("match");
+	openCard.classList.add("match");
+	openCards = [];
+
+	if(locks === shuffledCards.length/2) {
+	  	document.querySelector("#mod").style.display = "block";
+	  	document.querySelector(".congos").innerHTML =  "Congratulations. You won!!"
+	  	document.querySelector(".score").innerHTML =  "Game Over!! Your Score is: " +
+	  	 							(moves*10) + " in " + (60-time) + " sec " +
+  			 							"with " + rates + " rating." ;
+  		stop = 1;
+  	}
+}
+
+// set number of moves
+function updateMovesAndStars() {
+	var move = document.getElementById("moves");
+	move.innerHTML = moves;
+	let star = document.querySelectorAll(".stars li");
+	if(moves >20) {
+			star[2].style.color = "#fff";
+			rates = 2;
+	}
+	if(moves >40) {
+			star[1].style.color = "#fff";
+			rates = 1;
+	}
+	if(moves >60) {
+			star[0].style.color = "#fff";
+			rates = 0;
+	}
+}
+
+// reload page function
+function refresh() {
+	var refresh = document.querySelector(".restart");
+	refresh.click();
+}
+
+// event listener on restart icon when clicked reloads the page
+document.querySelector(".restart").addEventListener('click', function() {
+	window.location.reload(true);
+});
+
+// reload game when play again on modal is clicked
+document.querySelector(".repeat").addEventListener('click', function() {
+	window.location.reload(true);
+});
+
+// When user clicks on the main window, modal disappears and control goes back to window
+var modal = document.getElementById('mod');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
